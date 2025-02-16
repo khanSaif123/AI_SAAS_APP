9204e0aa4a9bfe6080c09367e90632e5ceb3c3e0
khanSaif123 <khansaiflko0@gmail.com>
2025-02-16 11:36:16 +0530

    update firebase admin

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,13 +1,17 @@
 import {initializeApp, getApp, getApps, App, cert} from "firebase-admin/app"
 import {getFirestore} from "firebase-admin/firestore"
 
-const serviceKey = require('@/service_key.json')
+// const serviceKey = require('@/service_key.json')
 
 let app: App;
 
 if(getApps().length === 0){
     app = initializeApp({
-        credential: cert(serviceKey)
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        })
     })
 }else{
     app = getApp()
