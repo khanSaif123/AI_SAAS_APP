d73c77217b6db8abebe00adc1cc1e9fa92e0dcbc
khanSaif123 <khansaiflko0@gmail.com>
2025-02-16 13:00:34 +0530

    next.cofig change

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,37 +1,33 @@
-import React from 'react'
-import {auth} from '@clerk/nextjs/server';
-import { adminDb } from '@/firebaseAdmin';
-import PdfView from '@/components/PdfView';
-import Chat from '@/components/Chat';
+import Chat from "@/components/Chat";
+import PdfView from "@/components/PdfView";
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
 
-const ChatToFilePage = async (props: {
-  params : {
-      id: string;
-  }
-}) => {
-  
-  auth.protect(); // if user is logged in then only listen to the request.
+async function ChatToFilePage({
+  params: { id },
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  auth.protect();
+  const { userId } = await auth();
 
-    // Destructure params after authentication
-  const { id } = await props.params;
-  
-  const {userId} = await auth()
-
-  // get the download url or actual pdf.
   const ref = await adminDb
-  .collection('users')
-  .doc(userId!)
-  .collection('files')
-  .doc(id)
-  .get()
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
 
   const url = ref.data()?.downloadUrl;
+
   return (
     <div className="grid lg:grid-cols-5 h-full overflow-hidden">
       {/* Right */}
       <div className="col-span-5 lg:col-span-2 overflow-y-auto">
         {/* Chat */}
-        <Chat id={id}/>
+        <Chat id={id} />
       </div>
 
       {/* Left */}
@@ -40,7 +36,6 @@ const ChatToFilePage = async (props: {
         <PdfView url={url} />
       </div>
     </div>
-  )
+  );
 }
-
-export default ChatToFilePage
+export default ChatToFilePage;
